f26954d0565664b2eed6900299335b9deef675c1
autumn <499747554@qq.com>
2017-10-31 20:39:32 +0800

    根据id获取用户信息

---
+++ src/main/java/com/leaf/club/model/User.java
@@ -0,0 +1,115 @@
+package com.leaf.club.model;
+
+/**
+ * @author xiechao
+ */
+public class User {
+    private int id;
+    private long registeredTime;
+    private String userName;
+    private byte[] userImg;
+    private String userEmail;
+    private String userPhone;
+    private int userAge;
+    private int userSex;
+    private int userStatus;
+    private String userNickName;
+    private String userAddress;
+    private String userIp;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getRegisteredTime() {
+        return registeredTime;
+    }
+
+    public void setRegisteredTime(long registeredTime) {
+        this.registeredTime = registeredTime;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public byte[] getUserImg() {
+        return userImg;
+    }
+
+    public void setUserImg(byte[] userImg) {
+        this.userImg = userImg;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public int getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(int userAge) {
+        this.userAge = userAge;
+    }
+
+    public int getUserSex() {
+        return userSex;
+    }
+
+    public void setUserSex(int userSex) {
+        this.userSex = userSex;
+    }
+
+    public int getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(int userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    public String getUserNickName() {
+        return userNickName;
+    }
+
+    public void setUserNickName(String userNickName) {
+        this.userNickName = userNickName;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public String getUserIp() {
+        return userIp;
+    }
+
+    public void setUserIp(String userIp) {
+        this.userIp = userIp;
+    }
+}
